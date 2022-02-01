f21b1950a9b21c439b146d9e72b8dbde6d914061
Kirill Ragozin <ragozin@hey.com>
2022-02-01 20:55:15 +0900

    feat: add tasks to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+#  ========= INITIALIZE TRUCK ============
+
+truck = Truck.create(name: 'The Scoop Bus')
+p "Created a truck (id: #{truck.id})"
+
+#  ========= INITIALIZE ICE CREAM PRODUCTS ============
+
+category = ProductCategory.create(name: 'Ice Cream')
+p "Created #{category.name} category"
+
+ice_cream_flavors = ['Chocolate', 'Pistachio', 'Strawberry', 'Mint']
+ice_cream_flavors.each do |flavor|
+  product = Product.create(category: category, name: "#{flavor} Ice Cream", price: rand(1.00..10.00))
+  p "Created '#{product.name}' product"
+  quantity = 10
+  truck.inventory.add(product.id, quantity)
+  p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
+end
+
+#  ========= INITIALIZE SHAVED ICE PRODUCTS ============ 
+
+category = ProductCategory.create(name: 'Shaved Ice')
+p "Created #{category.name} category"
+product = Product.create(category: category, name: "Kakigori Shaved Ice", price: rand(1.00..10.00))
+p "Created '#{product.name}' product"
+quantity = 10
+truck.inventory.add(product.id, quantity)
+p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
+
+
+#  ========= INITIALIZE SNACK PRODUCTS ============
+
+category = ProductCategory.create(name: 'Snack')
+p "Created #{category.name} category"
+
+product = Product.create(category: category, name: "Caramel Bar", price: rand(1.00..10.00))
+p "Created '#{product.name}' product"
+quantity = 10
+truck.inventory.add(product.id, quantity)
+p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
