8f24dcc63b078702617368a717849115bbb16357
Kirill Ragozin <ragozin@hey.com>
2022-02-01 19:13:33 +0900

    feat: add initial testing configs

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Icecreamtruck
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +19,19 @@ module Icecreamtruck
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "UTC"
+    config.eager_load_paths << Rails.root.join("lib/modules")
+    
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixtures false
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helpers false
+    end
   end
 end
