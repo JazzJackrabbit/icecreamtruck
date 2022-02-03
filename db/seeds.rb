383a7730ab667c5c62ce1b110a9aee128100aff2
Kirill Ragozin <ragozin@hey.com>
2022-02-03 22:22:13 +0900

    refactor: update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+
+
 #  ========= INITIALIZE TRUCK ============
 
 truck = Truck.create(name: 'The Scoop Bus')
 p "Created a truck (id: #{truck.id})"
+
+
 
 #  ========= INITIALIZE ICE CREAM PRODUCTS ============
 
@@ -15,10 +19,12 @@ ice_cream_flavors = ['Chocolate', 'Pistachio', 'Strawberry', 'Mint']
 ice_cream_flavors.each do |flavor|
   product = Product.create(category: category, name: "#{flavor} Ice Cream", price: rand(1.00..10.00))
   p "Created '#{product.name}' product"
-  quantity = 10
+  quantity = 200
   truck.inventory.add(product.id, quantity)
   p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
 end
+
+
 
 #  ========= INITIALIZE SHAVED ICE PRODUCTS ============ 
 
@@ -26,9 +32,23 @@ category = ProductCategory.create(name: 'Shaved Ice')
 p "Created #{category.name} category"
 product = Product.create(category: category, name: "Kakigori Shaved Ice", price: rand(1.00..10.00))
 p "Created '#{product.name}' product"
-quantity = 10
+quantity = 80
 truck.inventory.add(product.id, quantity)
 p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
+
+
+
+#  ========= INITIALIZE SNACK PRODUCTS ============
+
+category = ProductCategory.create(name: 'Snack')
+p "Created #{category.name} category"
+
+product = Product.create(category: category, name: "Caramel Bar", price: rand(1.00..10.00))
+p "Created '#{product.name}' product"
+quantity = 50
+truck.inventory.add(product.id, quantity)
+p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
+
 
 
 #  ========= INITIALIZE SNACK PRODUCTS ============
@@ -41,3 +61,24 @@ p "Created '#{product.name}' product"
 quantity = 10
 truck.inventory.add(product.id, quantity)
 p "Added #{quantity} units of #{product.name} to Truck##{truck.id} inventory"
+
+
+
+#  ========= INITIALIZE MERCHANT ============
+
+merchant = Merchant.create(email: 'merchant@icecreamtruck.xyz', password: '12345678', password_confirmation: '12345678')
+p "Created merchant account (#{merchant.email} / #{merchant.password})"
+
+
+
+#  ========= INITIALIZE ORDERS ============
+
+5.times do 
+  product = truck.products.sample
+  quantity = rand(1..5)
+  order = Order.create(truck: truck, items_attributes: [{
+    product_id: product.id,
+    quantity: quantity
+  }])
+  p "Created Order##{order.id} for truck (#{quantity} units of #{product.name} for $#{order.total_amount}"
+end
