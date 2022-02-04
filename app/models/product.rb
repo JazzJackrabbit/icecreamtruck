4729e024bd756b4efd2e78063ab5675481fc3274
Kirill Ragozin <ragozin@hey.com>
2022-02-04 18:49:32 +0900

    fix: remove potential N+1 query from stocked_in_truck product scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
 
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 
-  scope :stocked_in_truck, ->(truck_id) { includes(:inventories).where(inventories: { truck_id: truck_id.to_i, quantity: 1.. })}
+  scope :stocked_in_truck, ->(truck_id) { includes(:category).includes(:inventories).where(inventories: { truck_id: truck_id.to_i, quantity: 1.. })}
 
   scope :by_label, ->(label) { where(':label = ANY(labels)', label: label) }
 
