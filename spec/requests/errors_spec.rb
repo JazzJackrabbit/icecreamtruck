497d8012d2cef115feed114f9d3ef7f9b88b4a3c
Kirill Ragozin <ragozin@hey.com>
2022-02-04 18:33:06 +0900

    fix: nicer 404 and 500 pages

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe "Web Errors", type: :request do
       get "/qwerty"
       expect(response).to have_http_status(:not_found)
     end
+
+    it "has a page for an internal server error route" do
+      get '/500'
+      expect(response).to have_http_status(:internal_server_error)
+    end
   end
 end
